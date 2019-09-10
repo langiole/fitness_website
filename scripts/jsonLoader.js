9d8845925a950836cff948b14e7e69999a9f1604
Chris Mayol <drlilchris@gmail.com>
2019-09-10 16:51:42 -0400

    dynamically loaded exercises

--- scripts/jsonLoader.js
+++ scripts/jsonLoader.js
@@ -1,12 +1,40 @@
-const fs= require('fs');
-
-       
-            const jsonStr =fs.readFileSync('data/abs.json');
-            const exerciseArr= JSON.parse(jsonStr);
-
-            exerciseArr.abs.forEach(element => {
-                console.log(element.Exercise);
+$.getJSON("data/abs.json",exerciseArr=>{
+    $(document).ready(function(){
+    generateExerciseHTML(exerciseArr);
+    generateTable(exerciseArr);
+    });
+});
+function generateExerciseHTML(exerciseArr){
+    let index=window.location.search.substr(1);
+    if(index =="") index=0;
+    let muscle= Object.getOwnPropertyNames(exerciseArr)[0];
+    document.getElementById("exerciseTitle").innerText=exerciseArr.abs[index].Exercise;
+    document.getElementById("descrp").innerText=exerciseArr.abs[index].Descriptions;
+    document.getElementById("yt_video").setAttribute("src",exerciseArr.abs[index].link);
+    document.getElementById("buttonText").innerText=`Show more ${muscle} exercises`;
+    document.getElementById("tableTitleId").innerText=`${muscle} Exercises`;
+}
+            
+function generateTable(exerciseArr){
+            let table= document.getElementById("exerTable");
+            
+            exerciseArr.abs.forEach((element,counter) => {
+            
+                    let tr= document.createElement("tr");
+                    
+                    for(let i=0; i<5; i++){
+                    let td= document.createElement("td");
+                    let a= document.createElement("a");
+                    a.href=`?${counter}`;
+                    
+                    a.innerText = element.Exercise;
+                    td.appendChild(a);
+                    tr.appendChild(td);
+                    }
+                    table.appendChild(tr);
+                
             });
+}
             
 
 
