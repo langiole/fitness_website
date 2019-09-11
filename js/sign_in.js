ca277945a8163ef1f56d8b5c0b3f645b07624c74
Kevin Ludlam <kevinlud92@gmail.com>
2019-09-11 19:40:48 -0400

    end of day

--- js/sign_in.js
+++ js/sign_in.js
@@ -14,3 +14,10 @@ function CheckMember()
             return false; 
         }
 }
+
+function showAlert(){
+  if($("#myAlert").find("div#myAlert2").length==0){
+    $("#myAlert").append("<div class='alert alert-success alert-dismissable' id='myAlert2'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button> Success! message sent successfully.</div>");
+  }
+  $("#myAlert").css("display", "");
+}
