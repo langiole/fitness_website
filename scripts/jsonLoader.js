cf83dcb7009f07a4c7769bbd9bff4390b90175cf
Chris Mayol <drlilchris@gmail.com>
2019-09-10 23:56:22 -0400

    Revert "Revert "fix table""
    This reverts commit a58d32adfc0e929fbcc5f528ba1fdee429d4bd0d.

--- scripts/jsonLoader.js
+++ scripts/jsonLoader.js
@@ -1,39 +1,45 @@
+//use jquery's getJSON function to load json file
 $.getJSON("data/abs.json",exerciseArr=>{
+    //wait for the DOM to load
     $(document).ready(function(){
     generateExerciseHTML(exerciseArr);
     generateTable(exerciseArr);
     });
 });
+
+//Manipulate and reload the DOM based on the query passed
 function generateExerciseHTML(exerciseArr){
     let index=window.location.search.substr(1);
     if(index =="") index=0;
     let muscle= Object.getOwnPropertyNames(exerciseArr)[0];
+
     document.getElementById("exerciseTitle").innerText=exerciseArr.abs[index].Exercise;
     document.getElementById("descrp").innerText=exerciseArr.abs[index].Descriptions;
     document.getElementById("yt_video").setAttribute("src",exerciseArr.abs[index].link);
     document.getElementById("buttonText").innerText=`Show more ${muscle} exercises`;
     document.getElementById("tableTitleId").innerText=`${muscle} Exercises`;
 }
-            
+//Dynamically manipulate the table in our DOM
 function generateTable(exerciseArr){
             let table= document.getElementById("exerTable");
             
-            exerciseArr.abs.forEach((element,counter) => {
-            
+            let i=0;
+            while(i <exerciseArr.abs.length){
                     let tr= document.createElement("tr");
                     
-                    for(let i=0; i<5; i++){
+                    for(let j=0; j<5; j++){
+                       if(!(i <exerciseArr.abs.length)) break;
+                    let element=exerciseArr.abs[i];
                     let td= document.createElement("td");
                     let a= document.createElement("a");
-                    a.href=`?${counter}`;
-                    
+                    a.href=`?${i}`;
                     a.innerText = element.Exercise;
                     td.appendChild(a);
                     tr.appendChild(td);
+                    i++;
                     }
                     table.appendChild(tr);
-                
-            });
+            }
 }
             
 
