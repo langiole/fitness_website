022a1938aec06596c48c4983e2cfd952fecd7447
Lee Angioletti <langio@outlook.com>
2019-09-24 15:49:21 -0400

    initial spring boot

---
+++ spring-boot/src/main/java/com/collabera/FeMan/FeManApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.FeMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeManApplication.class, args);
+	}
+
+}
