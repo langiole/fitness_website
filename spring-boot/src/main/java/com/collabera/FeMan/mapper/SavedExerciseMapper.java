833b8b86b5ea3543187bbfcfe335a0905e4db09b
Kevin Ludlam <kevinlud92@gmail.com>
2019-09-25 11:15:09 -0400

    sorry mean it this time

---
+++ spring-boot/src/main/java/com/collabera/FeMan/mapper/SavedExerciseMapper.java
@@ -0,0 +1,29 @@
+package com.collabera.FeMan.mapper;
+
+import org.springframework.stereotype.Component;
+
+
+import com.collabera.FeMan.dto.SavedExerciseDTO;
+import com.collabera.FeMan.model.SavedExercise;
+
+@Component
+public class SavedExerciseMapper 
+{
+	public SavedExercise toEntity(SavedExerciseDTO dto)
+	{
+		SavedExercise saved = new SavedExercise();
+		saved.setId(dto.getId());
+		saved.setUser_id(dto.getUser_id());
+		saved.setExercise_id(dto.getExercise_id());
+	}
+	
+	public SavedExerciseDTO toDto(SavedExercise entity)
+	{
+		SavedExerciseDTO saved = new SavedExerciseDTO();
+		saved.setId(entity.getId());
+		saved.setUser_id(entity.getUser_id());
+		saved.setExercise_id(entity.getExercise_id());
+	}
+}
+
+
