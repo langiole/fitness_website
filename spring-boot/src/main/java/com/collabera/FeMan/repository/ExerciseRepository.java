de6edaae4d32adb2c4a18f5841b6c9447eb9659b
Chris Mayol <drlilchris@gmail.com>
2019-09-25 16:16:08 -0400

    Revert "Merge branch 'spring-boot-development' of https://github.com/langiole/fitness_website into spring-boot-development"
    This reverts commit fc6c655c4115f72e0c8c4f49becf29706037fe0b, reversing
    changes made to 17376329c695c101f89f0c4e4de265ff6fee5b7a.

--- spring-boot/src/main/java/com/collabera/FeMan/repository/ExerciseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.collabera.FeMan.repository;
-
-import com.collabera.FeMan.model.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
-}
