bc6b5a3fefaf95632e152bc8496ba8395796fd59
Kevin Ludlam <kevinlud92@gmail.com>
2019-09-25 09:31:20 -0400

    inshal dto mapper update

--- spring-boot/src/main/java/com/collabera/FeMan/FeManApplication.java
+++ spring-boot/src/main/java/com/collabera/FeMan/FeManApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FeManApplication {
+public class FeManApplication 
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeManApplication.class, args);
