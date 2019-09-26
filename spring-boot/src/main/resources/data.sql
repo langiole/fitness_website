878aa800a97397fe531b640829279aefd1ba1b22
Chris Mayol <drlilchris@gmail.com>
2019-09-26 14:06:51 -0400

    adding static files

--- spring-boot/src/main/resources/data.sql
+++ spring-boot/src/main/resources/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO Users (first_name,last_name,email, password, date_registered)
-	Values("John","Snow","JohnSnow@gmail.com","mypassword", "2019-06-15");
+	Values('John','Snow','JohnSnow@gmail.com','mypassword', '2019-06-15');
 
 INSERT INTO Users (first_name,last_name,email, password, date_registered)
-	Values("Steve","White","SteveWhite@gmail.com","mypassword", "2019-07-10");
+	Values('Steve','White','SteveWhite@gmail.com','mypassword', '2019-07-10');
 	
 INSERT INTO Exercises (name, description, primary_muscle, secondary_muscle, video_link)
-	Values("alternating floor press",
-    "Lie on the floor with either two dumbbells or two kettlebells on your hand. Press one arm at a time, while the other hand is stationary. Lower your pressing hand until either your elbow or triceps touches the floor. Press the weight back up and repeat with the other hand.",
-    "chest","arms","https://www.youtube.com/embed/Qb-vUPub-RQ");
+	Values('alternating floor press',
+    'Lie on the floor with either two dumbbells or two kettlebells on your hand. Press one arm at a time, while the other hand is stationary. Lower your pressing hand until either your elbow or triceps touches the floor. Press the weight back up and repeat with the other hand.',
+    'chest','arms','https://www.youtube.com/embed/Qb-vUPub-RQ');
     
  
